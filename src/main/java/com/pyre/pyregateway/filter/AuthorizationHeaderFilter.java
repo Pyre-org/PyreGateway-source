98f4c5b82861ce1e77551d11d0cab74cc3bfc459
wodn1478 <wodn14789@naver.com>
2024-04-01 16:51:21 +0900

    feat : 401 발생 시 에러 메세지 전송
    액세스 토큰이 만료 되었을 때 body에 에러 메세지를 전송하도록 메세지 추가

--- src/main/java/com/pyre/pyregateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/pyre/pyregateway/filter/AuthorizationHeaderFilter.java
@@ -1,5 +1,7 @@
 package com.pyre.pyregateway.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -11,6 +13,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -18,10 +21,14 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Flow;
 
 
 @Component
@@ -54,7 +61,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             }
             if (!isJwtValid(jwt)) {
                 log.info("jwt 검증 실패");
-                return onError(exchange,"JWT token is not valid",HttpStatus.UNAUTHORIZED);
+                return onError(exchange,"JWT token is not valid", HttpStatus.UNAUTHORIZED);
             }
 
             String id = String.valueOf(Jwts.parserBuilder().setSigningKey(secret).build()
@@ -84,9 +91,24 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ObjectMapper om = new ObjectMapper();
+
         ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(httpStatus);
-        return response.setComplete();
+
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+
+        // JSON 응답을 보낼 경우
+        response.getHeaders().add("Content-Type", "application/json");
+        Map<String, String> body = Map.of("message", "Unauthorized", "code", "401", "success", "false");
+
+        try {
+            DataBuffer buffer = response.bufferFactory().wrap(om.writeValueAsBytes(body));
+
+            return response.writeWith(Mono.just(buffer));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
